0c905fab2aaeac99c82df2f9eb5e7b35cc070e6b
jdsong <jdsong@lewatek.com>
2013-08-21 22:44:10 +0800

    remove unused code

--- gen/com/lewa/droidtest/BuildConfig.java
+++
@@ -1,9 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-/** Automatically generated file. DO NOT MODIFY */
-
-package com.lewa.droidtest;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- src/com/lewa/droidtest/memory/MemoryClearance.java
+++
@@ -1,7 +0,0 @@
-package com.lewa.droidtest.memory;
-
-import android.app.Fragment;
-
-public class MemoryClearance extends Fragment{
-        
-}
